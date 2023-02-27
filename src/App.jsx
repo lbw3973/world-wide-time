20a1b30136b4ca3b34c2f432cf701b05b2b4699a
LeeByoungWook <lbw3973@naver.com>
2023-02-27 19:38:28 +0900

    Feat: Reset버튼 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,11 @@ function App() {
     <div className="main">
       <div className="header">
         <h1> World Wide Time</h1>
-        <button onClick={resetDatas}>전부 되돌리기</button>
       </div>
       <Clock />
+      <div className="btnBox">
+        <button onClick={resetDatas}>전부 되돌리기</button>
+      </div>
     </div>
   );
 }
